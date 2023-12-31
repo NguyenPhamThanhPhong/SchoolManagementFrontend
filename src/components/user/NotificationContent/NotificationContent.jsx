d20aa4a3922c956417509827f875491605af9ed3
Trần Quang Huy <huy.com2003@gmail.com>
2023-12-31 22:16:32 +0700

    Notification html string

--- src/components/user/NotificationContent/NotificationContent.jsx
+++ src/components/user/NotificationContent/NotificationContent.jsx
@@ -1,6 +1,9 @@
+import parse from 'html-react-parser';
+
 function NotificationContent() {
     const Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac. Senectus et netus et malesuada. Nunc pulvinar"
     const Time = "11:25am";
+    const ContentHtml = '<h1>Hello</h1><div>div test</div>';
     return (
         <>
             <div>
@@ -8,6 +11,7 @@ function NotificationContent() {
             </div>
             <div>
                 {Content}
+                {parse(ContentHtml)}
             </div>
         </>
     )
