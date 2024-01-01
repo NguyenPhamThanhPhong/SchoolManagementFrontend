b697ee459df8b463af7472813b8c2f54cdc41370
Trần Quang Huy <huy.com2003@gmail.com>
2024-01-01 09:55:56 +0700

    Add Notification

--- src/pages/user/Home/Home.jsx
+++ src/pages/user/Home/Home.jsx
@@ -1,11 +1,32 @@
 import './Home.css'
 import NotificationList from "../../../components/user/NotificationList/NotificationList";
+import { Tabs } from 'antd';
 
+const onChange = (key) => {
+  console.log(key);
+};
+const items = [
+  {
+    key: '1',
+    label: 'Faculty notification',
+    children: <NotificationList NotiType={'Faculty notification'}></NotificationList>,
+  },
+  {
+    key: '2',
+    label: 'General notification',
+    children: <NotificationList NotiType={'General notification'}></NotificationList>,
+  },
+  {
+    key: '3',
+    label: 'Your notification',
+    children: <NotificationList NotiType={'Your notification'}></NotificationList>,
+  },
+];
 function Home() {
   return (
     <>
       <div className="homeMain">
-        <NotificationList></NotificationList>
+        <Tabs className='TabsPosition' defaultActiveKey="1" items={items} onChange={onChange} />
       </div>
     </>
   );
